d6e07decd7a50d48f35cb9f99290aef93d92b4d8
ayush2300 <ayushgosavi230@gmail.com>
2025-06-25 17:24:32 +0530

    PlayerMovemnet

--- Assets/DevA/Script/Player/PlayerControlle.cs
+++ Assets/DevA/Script/Player/PlayerControlle.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerControlle: MonoBehaviour
+public class PlayerControlle: MonoBehaviour , IDamageAble , IHealAble
 {
+
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed;
+    private Vector2 move;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.velocity = new Vector2 (move.x * speed, move.y * speed);
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+
     }
 }
